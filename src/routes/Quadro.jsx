6f2a47535e98362a1f87a65f0cd1cd9ef50ddb33
Maiq <maique1915@gmail.com>
2024-01-20 04:28:47 -0300

    Update Quadro.jsx

--- src/routes/Quadro.jsx
+++ src/routes/Quadro.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 import { ativas } from '../model/Filtro'
 import Comum from './Comum'
 
-class Quadro extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { cur: props.cur }
-	}
-
-	render() {
-		const a = ativas(this.state.cur)
-		return <Comum materias={[a]} tela={1} cur={this.state.cur} separa={true} g={"º"} f={' Período'} />
-	}
+const Quadro = ({ cur }) => {	
+	const a = ativas(cur)
+	console.log(cur)
+	return <Comum materias={[a]} tela={1} cur={cur} separa={true} g={"º"} f={' Período'} />
 }
 
 export default Quadro
