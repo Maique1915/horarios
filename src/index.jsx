c8785008244c65f4c43250d2a9d3b130d115935d
Maique Pereira <maique1915@gmail.com>
2025-11-10 01:20:04 +0000

    feat: Corrige caminho base para '/Matricula/' na configuração do roteador

--- src/index.jsx
+++ src/index.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
         }
     ],
     {
-        basename: '/Matricula' // 👈 Importante para GitHub Pages
+        basename: '/Matricula/' // 👈 Importante para GitHub Pages
     }
 );
 
