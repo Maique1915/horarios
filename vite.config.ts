ae80aecdc21938fa6c35c7b4c5dd3ec2c2ee99be
maique <maique1915@gmail.com>
2025-12-18 17:11:21 -0300

    Fix .gitignore and push missing root path configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Matricula/',
+  base: '/',
   build: {
     outDir: 'dist'
     // NÃO inclua 404.html aqui
