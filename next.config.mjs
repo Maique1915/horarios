ab8334952d6495147bfd8f96d8f492d7adc52beb
maique <maique1915@gmail.com>
2025-12-23 14:36:40 -0300

    Fix Vercel deployment: Conditional basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
     reactStrictMode: true,
-    basePath: '/horarios',
-    output: 'export',
+    basePath: isGithubPages ? '/horarios' : undefined,
+    output: isGithubPages ? 'export' : undefined,
     images: {
         unoptimized: true,
     },
